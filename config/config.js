3a4b3996f57f5598bd5b1ed9bf430af31808dbc3
VioniWijaya <viowijayaputri@gmail.com>
2025-02-07 09:24:00 +0700

    connect

--- config/config.js
+++ config/config.js
@@ -15,10 +15,16 @@ module.exports = {
     "dialect": process.env.DB_CONNECTION
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_CONNECTION
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "dialect": process.env.DB_CONNECTION,
+  "dialectOptions": {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+    }
   }
+ }
 }
